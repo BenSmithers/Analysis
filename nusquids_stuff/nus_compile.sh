708345b85778de87d216f4a66f5abcec95feba18
Benjamin Smithers <benjamin.smithers@mavs.uta.edu>
2020-03-16 12:09:24 -0500

    short bash script to compile individual scripts into binary files

---
+++ nusquids_stuff/nus_compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# short script to make it easier to compile individual nusquids scripts 
+# arg1 - file to compile
+# arg2 - output binary file
+
+if [ -z "$1" ]
+then
+    echo "Nothing given to compile!"
+    exit 1
+else
+    if [ -z "$2" ]
+    then 
+        echo "Specify output file!"
+        exit 1
+    else
+        g++ -std=c++11 $1 -I./ -I$SROOT/include -I$GOLEMSPACE/include -L$SROOT/lib -L$SROOT/lib64 -L$GOLEMSPACE/local/lib -I$GOLEMSPACE/local/lib64 -lSQuIDS -lnuSQuIDS -lhdf5_hl -lhdf5 -lgsl -lgslcblas -o $2
+    fi
+fi
+
+
