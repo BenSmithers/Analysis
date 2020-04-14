d0f2c50b9b65fc98546b0988c55a20c174f9b204
Benjamin Smithers <benjamin.smithers@mavs.uta.edu>
2020-04-14 15:47:12 -0500

    copied over some NSI examples from nuSQuIDS

---
+++ nusquids_stuff/atm_h5.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+
+#include <nyuSQuIDS/nuSQuIDS.h>
+/*
+     + nusquids stuff
+     + squids stuff
+*/
+
+namespace nusquids{
+
+int main(){
+    squids::Const units;
+
+
+}
+
+}; // namespace nusquids
