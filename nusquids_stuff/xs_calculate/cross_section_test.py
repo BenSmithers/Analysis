a2f41e5f3291c3ff439b739cd8a0257839670bb5
Benjamin Smithers <benjamin.smithers@mavs.uta.edu>
2020-04-22 10:46:04 -0500

    rename dis.py to make more sense

--- nusquids_stuff/xs_calculate/cross_section_test.py
+++ nusquids_stuff/xs_calculate/cross_section_test.py
@@ -2,8 +2,11 @@
 This script will calcualte the DIS cross sections for neutrinos and convolve that with an expected flux in the ice 
 """
 
-import nuSQuIDSpy as nsq
+import nuSQUIDSpy as nsq
 import numpy as np # useful for energy ranges
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 
 constants = nsq.Const()
 
@@ -31,7 +34,7 @@ xs_obj = nsq.NeutrinoDISCrossSectionsFromTables()
 
 
 
-def get_diff_flux( energy, flavor, neutrino, current, y=None, x=None):
+def get_diff_flux( energy, flavor, neutrino, current, e_out=None, x=None):
     """
     This function is just a wrapper to the nusquids function to make sure I get my types right.... 
     """
@@ -44,14 +47,64 @@ def get_diff_flux( energy, flavor, neutrino, current, y=None, x=None):
     if not isinstance( neutrino, nsq.NeutrinoCrossSections_NeutrinoType):
         raise TypeError("Expected {} for neutrino type, got {}".format(nsq.NeutrinoCrossSections_NeutrinoType, type(neutrino)))
 
-    if (y is None) or (x is None):
+    if (e_out is None) or (x is None):
         return( xs_obj.TotalCrossSection( energy, flavor, neutrino, current))
     else:
-        if not (isinstance(y,float) or isinstance(y,int)):
-            raise TypeError("Expected {} for y, got {}".format(float, type(y)))
+        if not (isinstance(e_out,float) or isinstance(e_out,int)):
+            raise TypeError("Expected {} for e_out, got {}".format(float, type(e_out)))
         if not (isinstance(x,float) or isinstance(x,int)):
             raise TypeError("Expected {} for x, got {}".format(float, type(x)))
-        return(xs_obj.
+        return(xs_obj.SingleDifferentialCrossSection( energy, e_out, flavor, neutrino, current))
 
 def get_total_flux( energy, flavor, neutrino, current):
     return(get_diff_flux(energy, flavor, neutrino, current))
+
+total_xss = {}
+for current in currents:
+    for nt in neut_types:
+        for flavor in flavors:
+            cross = np.array([ get_total_flux( en, flavors[flavor], neut_types[nt], currents[current]) for en in energies])
+            total_xss[current+'_'+nt+'_'+flavor] = cross 
+
+
+muon_tracks = np.array([0. for i in energies])
+muon_cascades = np.array([0. for i in energies])
+for current in currents:
+    for nt in neut_types:
+        for flavor in flavors:
+            if current=='CC':
+                if flavor=='muon':
+                    muon_tracks += 0.5*total_xss[current+'_'+nt+'_'+flavor]
+            else:
+                if flavor=='muon':
+                    muon_cascades += 0.5*total_xss[current+'_'+nt+'_'+flavor]
+
+
+print("plotting")
+
+plt.plot(energies, muon_cascades,label="muon cascades")
+plt.plot(energies, muon_tracks,label='muon tracks')
+
+plt.yscale('log')
+plt.xscale('log')
+plt.xlabel('Neurino Energy [eV]',size=16)
+plt.ylabel(r'Total Cross Section [cm$^{2}$]',size=16)
+plt.legend()
+plt.savefig('test.png',dpi=400)
+plt.clf()
+
+# Let's try fixing the reconstructed energy 
+# and seeing how the differential cross section looks! 
+recon_energy = 100*constants.TeV
+by_min = -5
+by_max = 0
+in_energies = np.array([recon_energy/(1.-y) for y in (1-np.logspace(by_min, by_max, nBins))])
+diff_xs = [ get_diff_flux(energy, flavors['muon'],neut_types['neutrino'],currents['NC'],recon_energy,0.) for energy in in_energies ]
+
+plt.plot(in_energies/constants.GeV, diff_xs)
+plt.xscale('log')
+plt.yscale('log')
+plt.title("Differential Cross Section from {:.2f} TeV Muon".format(recon_energy/constants.TeV))
+plt.xlabel("Muon Neutrino Energy [GeV]")
+plt.ylabel("Diffy Xs")
+plt.savefig('test2.png',dpi=400)
