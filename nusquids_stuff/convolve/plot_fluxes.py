fafd45d571e6d6c7dc178e0c2f7261e995ead3a3
Benjamin Smithers <benjamin.smithers@mavs.uta.edu>
2020-04-30 13:40:31 -0500

    plots the (y vs incident) energy plot, as we desire

--- nusquids_stuff/convolve/plot_fluxes.py
+++ nusquids_stuff/convolve/plot_fluxes.py
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt
 from cross_section_test import get_diff_flux
 import os
 import nuSQUIDSpy as nsq
+import sys
+
+const = nsq.Const()
 
 # colormap thing
 cmap = plt.get_cmap('viridis')
@@ -99,6 +102,16 @@ assert( len(data) == (n_energies*n_angles))
 # it loops through energies for each angle
 print("Building Energy and Angle Arrays")
 energies = [data[i][0] for i in range(n_energies)]
+if energies[1]>energies[0]:
+    print("growing")
+else:
+    print("decreasing")
+en_width = [0. for i in range(n_energies)]
+for i in range(n_energies):
+    if i==0:
+        en_width[i] = (10**energies[1]- 10**energies[0])/const.GeV
+    else:
+        en_width[i] = (10**energies[i] - 10**energies[i-1])/const.GeV
 angles = [data[n_energies*i][1] for i in range(n_angles)]
 
 # let's fill out some flux functions
@@ -114,16 +127,72 @@ for flav in flavors:
             key = flav+'_'+neut + '_'+curr
             if flav=='Mu' and curr=='CC':
                 continue 
+            scale_with = 1.
+#            scale_with = get_diff_flux((10**energies[energy]), get_flavor(key), get_neut(key), get_curr(key))
 
-            fluxes[ key ] = [sum([ data[energy+angle*n_energies][get_index(key)] for angle in range(n_angles)])*get_diff_flux(energy, get_flavor(key), get_neut(key), get_curr(key)) for energy in range(n_energies)]
+            fluxes[ key ] = [sum([ data[energy+angle*n_energies][get_index(key)] for angle in range(n_angles)])*scale_with*(en_width[energy])*4*np.pi for energy in range(n_energies)]
             
             if curr=='NC' and flav=='Mu':
                 muon_ones += np.array(fluxes[key])
             else:
                 not_muon += np.array(fluxes[key])
 
+def get_flux( energy, key, use_overflow = False):
+    '''
+    interpolates between entries in the flux dictionary to return the flux at arbitrary energy
+    interpolation done in log-space of energy! 
+
+
+    returns NONE if energy is beyond edges of flux data
+    returns DOUBLE otherwise 
+    '''
+    if not (key in fluxes):
+        raise ValueError("Bad key {}".format(key))
+    if not (isinstance(energy, float) or isinstance(energy, int)):
+        raise TypeError("Expected {}, not {}".format(float, type(energy)))
+
+    
+
+    # check if it's outside the extents
+    if energy < energies[0]:
+        if use_overflow:
+            return(energies[0])
+        else:
+            return(0)
+    if energy > energies[-1]:
+        if use_overflow:
+            return(energies[n_energies - 1])
+        else:
+            return(0)
+    
+    # should execute in O(N) time 
+    upper_boundary = 1
+    while energy>energies[upper_boundary]:
+        upper_boundary += 1
+    lower_boundary = upper_boundary - 1
+
+    # sanity check... 
+    # essentially makes sure that the energies are monotonically increasing 
+    if not ((energies[lower_boundary] <= energy) and (energies[upper_boundary] >= energy)):
+        print("energy: {}".format(energy))
+        print("lower bound: {}".format(energies[lower_boundary]))
+        print("upper bound: {}".format(energies[upper_boundary]))
+        print("indices: {}, {}".format(lower_boundary, upper_boundary))
+        raise Exception()
+
+    # logarithmic interpolation
+    # linear in log-energy space 
+    y2 = fluxes[key][upper_boundary]
+    y1 = fluxes[key][lower_boundary]
+    x2 = energies[upper_boundary]
+    x1 = energies[lower_boundary]
+    
+    flux_value = energy*((y2-y1)/(x2-x1) ) + y2 -x2*((y2-y1)/(x2-x1))
+    return(flux_value)
+
 scale_e = 10**np.array(energies)
 
+# this block here is supposed to just plot all the raw fluxes 
 plot_all = False
 if plot_all:
     it = 0
@@ -136,10 +205,68 @@ if plot_all:
     plt.yscale('log')
     plt.savefig('reallycool.png', dpi=400)
 
-# plot the stuff!
-plt.plot(scale_e, muon_ones, color=get_color(0,2), label="Muon Cascades")
-plt.plot(scale_e, not_muon, color=get_color(1,2), label="Other Cascades")
-plt.xscale('log')
-plt.yscale('log')
-plt.legend()
-plt.savefig('muon_not_muon.png', dpi=400)
+plt.clf()
+# this block here was supposed to compare muon cascade fluxes with non-muon cascade fluxes
+casc_compare = False
+if casc_compare:
+    plt.plot(scale_e, muon_ones, color=get_color(0,2), label="Muon Cascades")
+    plt.plot(scale_e, not_muon, color=get_color(1,2), label="Other Cascades")
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel("Incoming Neut Energy [GeV]")
+    plt.title("Event Rate for atm flux 10GeV->1PeV")
+    plt.ylabel("Event Rate [s$^{-1}$]")
+    plt.legend()
+    plt.savefig('muon_not_muon.png', dpi=400)
+plt.clf()
+
+
+# now I'm trying tofix what the final state energy is, and look at the relative rates of initial neutrino energy that would give rise to this event 
+cool_plot = True
+if cool_plot:
+    event = 100*const.GeV
+    by_min = -5
+    by_max = 0
+    n_bins = 200
+    # the initial neutrino energy we are considering
+    in_energies = np.array([event/(1.-y) for y in (1-np.logspace(by_min, by_max, n_bins))])
+    widths = [0. for i in range(n_bins)]
+    for i in range(n_bins):
+        if i==0:
+            widths[i]=abs(in_energies[1]-in_energies[0])/const.GeV
+        else:
+            widths[i]=abs(in_energies[i]-in_energies[i-1])/const.GeV
+
+    from_muon = np.array([ 0. for energy in range(n_bins) ])
+    from_not = np.array([ 0. for energy in range(n_bins) ])
+    from_diffy = {}
+    for flav in flavors:
+        for neut in neuts:
+            for curr in currents:
+                if flav=='Mu' and curr=='CC':
+                    continue 
+                   
+
+                key = flav+'_'+neut + '_'+curr
+                from_diffy[key] = [get_flux(np.log10(in_energies[j]), key)*get_diff_flux(in_energies[j], get_flavor(key), get_neut(key), get_curr(key), event,0.)*widths[j] for j in range(n_bins)]
+                # this evaluates in (N / GeV)
+                if curr=='NC' and flav=='Mu':
+                    from_muon += np.array(from_diffy[key])
+                else:
+                    from_not += np.array(from_diffy[key])
+    norm = sum(from_muon)+sum(from_not)
+    # these are properly normalized so that you can integrate over part of the trend to get the probability the event was of that type 
+    for i in range(n_bins):
+        from_muon[i]=from_muon[i]/(widths[i]*norm)
+        from_not[i] =from_not[i]/(widths[i]*norm)
+
+
+    plt.plot(in_energies/const.GeV, from_muon, color=get_color(0,2),label="Muon Origin")
+    plt.plot(in_energies/const.GeV, from_not, color=get_color(1,2),label="Other Origin")
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.title("{:.2f}GeV Cascade Rates".format(event/const.GeV))
+    plt.xlabel("Parent Neutrino Energy [GeV]")
+    plt.ylabel(r"Probability [GeV$^{-1}$]")
+    plt.legend()
+    plt.savefig("wow.png",dpi=400)
