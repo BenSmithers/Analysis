731c39cb8b6e181fccbb58073357eb126a5e82a8
Benjamin Smithers <benjamin.smithers@mavs.uta.edu>
2020-04-30 13:39:33 -0500

    switched to using mceq for the fluxes.

--- nusquids_stuff/convolve/convolve.cpp
+++ nusquids_stuff/convolve/convolve.cpp
@@ -10,16 +10,125 @@
 #include <fstream>
 #include <vector>
 #include <nuSQuIDS/nuSQuIDS.h>
-
+#include <array>
+#include <fstream>
+#include <math.h>
 using namespace nusquids;
 
 // this makes us consistent with the units we use
 squids::Const un; 
 
-double flux_function( double energy, double cos_zenith ){
+const int angular_bins = 2;
+const int energy_bins = 121;
+const double pi = 3.14159265359;
+
+// takes a string, returns a pointer to a length-4 array
+std::array<double, 4> parse( std::string line ){
+    std::array<double, 4> results = {0., 0., 0., 0.};
+    int index = 0;
+    
+    std::string working = "";
+    for (const char &c: line){
+        if (index>3){
+            std::cout << "Went too far??" <<std::endl;
+        }
+        if (c==' '){
+            results[index] = std::stod( working );
+            working = "";
+            index++;
+        }else{
+            working += c; 
+        }
+    }
+    // last character is probably not a space! 
+    results[index] = std::stod(working);
+    
+    return( results );
+
+}
+
+
+// interpolator for mceq fluxes
+// loads in data file and can 
+struct inter{
+    
+
+    double energies[energy_bins] = {};
+    double nue_fluxes[energy_bins] ={};
+    double numu_fluxes[energy_bins] = {};
+    double nutau_fluxes[energy_bins] = {};
+
+    void load_data(std::string which){
+
+        std::string holder; // holds a string while interpreting it into floats and stuff
+        std::ifstream target_data;
+        target_data.open( which );
+        int line = 0;
+        while(std::getline(target_data, holder)){
+//            std::getline( target_data, holder );
+            if(holder[0]=='#'){
+                continue;
+            }
+            std::array<double,4> values = parse( holder );
+            energies[line] = values[0];
+            nue_fluxes[line] = values[1];
+            numu_fluxes[line] = values[2];
+            nutau_fluxes[line] = values[3];
+            //std::cout << "Interpreted: "<<energies[line]<<", "<<nue_fluxes[line]<<", "<<numu_fluxes[line]<< ", " <<nutau_fluxes[line] << std::endl;
+
+            line++; 
+        }
+    }
+
+    double get_flux(double energy, uint8_t flavor ){
+        /*
+            This returns the flux of the given flavor at desired energy
+
+            It uses the loaded fluxes and linearly interpolates between the relevant points 
+         */
+
+        // return 0 if outside the energy range! 
+        if (energy < energies[0]){
+            return(0.);
+        }
+        if (energy > energies[energy_bins-1]){
+            return(0.);
+        }
+
+        uint16_t upper_b= 1;
+        while( energy > energies[upper_b] ){
+            upper_b += 1;
+        }
+        uint16_t lower_b = upper_b - 1;
+        
+        double y1,y2;
+        double x2 = energies[upper_b];
+        double x1 = energies[lower_b];
+
+        // access the relevant stored flux! 
+        if (flavor==0){
+            y2 = nue_fluxes[upper_b];
+            y1 = nue_fluxes[lower_b];
+        }else if(flavor==1){
+            y2 = numu_fluxes[upper_b];
+            y1 = numu_fluxes[lower_b];
+        }else if(flavor==2){
+            y2 = nutau_fluxes[upper_b];
+            y1 = nutau_fluxes[lower_b];
+        }
+
+        // linear interpolation 
+        double flux_value = energy*((y2-y1)/(x2-x1)) + y2 -x2*((y2-y1)/(x2-x1));
+        return flux_value;
+    }
+};
+
+
+double flux_function( double energy, double cos_zenith, uint8_t falvor){
     // energy should be in units of eV
     // cos_zenith should be, well, cos(zenith). 
     //       so between -1 and 1
+    // flavor: 0 e, 1 mu, 2 tau
 
     if (cos_zenith<-1 || cos_zenith>1){
         throw("Invalid cos_zenith received");
@@ -36,16 +145,25 @@ double flux_function( double energy, double cos_zenith ){
 }
 
 
+// this was just used to test thatthe interpolator was working right! 
+/*
 int main(){
+    inter Interpolator;
+    Interpolator.load_data("/home/bsmithers/software_dev/Analysis/fluxes/theta_0.dat");
+
+}*/
+
+
+int main(){
+    inter FluxMachine; 
+
     // define some properties for our atmosphere 
     double n_nu = 3;
     double Emin = (1.e1)*un.GeV;
     double Emax = (1.e6)*un.GeV;
-    double cos_zenith_min = -1.;
+    double cos_zenith_min = -0.999;
     double cos_zenith_max = 0.;
 
-    int angular_bins = 40;
-    int energy_bins = 100;
     bool use_earth_interactions = true;
     
     // create the atmosphere model
@@ -68,16 +186,28 @@ int main(){
     marray<double, 4> inistate{angular_bins, energy_bins, 2, n_nu};
     std::fill( inistate.begin(), inistate.end(), 0);
     for ( int angle_bin=0; angle_bin < angular_bins; angle_bin++){
+        // load next angle file  
+        double angle_deg = 180.-(acos(zeniths[angle_bin])*180./pi);
+        if (angle_deg > 180.){
+            angle_deg = 180.;
+        }
+        std::string command("python mceq_flux.py ");
+        command += std::to_string(angle_deg);
+        std::cout << " ===> Generating new flux file at "<<angle_deg<<std::endl;
+        system(command.c_str()); // call the python script to generate the flux
+        FluxMachine.load_data("temp_mceq_flux.dat"); // load the flux in 
+        std::cout << " ===> Assigning initial state " <<std::endl;
+        std::cout << std::endl;
         for (int energy_bin=0; energy_bin < energy_bins; energy_bin++){
             for (int neut_type =0; neut_type<2; neut_type++){
                 for (int flavor=0; flavor < n_nu; flavor++){
-                    inistate[angle_bin][energy_bin][neut_type][flavor] = flux_function( energies[energy_bin], zeniths[angle_bin] );
+                    inistate[angle_bin][energy_bin][neut_type][flavor] = 0.5*FluxMachine.get_flux( (1.e-9)*(pow(10,energies[energy_bin])), flavor);
                 }
             }
         }
     }
     nus_atm.Set_initial_state( inistate, flavor);
-    std::cout << "Done setting initial state" << std::endl;
+    std::cout << " ===> Done setting initial state" << std::endl;
 
     nus_atm.Set_ProgressBar(true);
     nus_atm.Set_IncludeOscillations(true);
@@ -96,6 +226,8 @@ int main(){
     // write header
     file << "# log10(energy) cos(zenith) flux_nuE flux_nuMu flux_nuTau flux_nuEBar flux_nuMuBar flux_nuTauBar" <<std::endl;
     for(double angle=cos_zenith_min; angle<cos_zenith_max; angle+=(cos_zenith_max-cos_zenith_min)/(double)int_cos){
+
+
         for(double energy= int_min_e; energy<int_max_e; energy+=(int_max_e-int_min_e)/(double)int_en){
             // write out the angle and the energy 
             file << energy << " " << angle;
@@ -132,3 +264,4 @@ int main(){
     return 0;
 
 }
+
