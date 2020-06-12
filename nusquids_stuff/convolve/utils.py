8c69e8cf9d704d0748240cbb69996c329b8d7d2e
Benjamin Smithers <benjamin.smithers@mavs.uta.edu>
2020-06-12 15:14:23 -0500

    wrote new binning system, still unimplemented elsewhere

--- nusquids_stuff/convolve/utils.py
+++ nusquids_stuff/convolve/utils.py
@@ -9,6 +9,80 @@ This defines a few utility functions for my plotting script.
 I moved this over here so the main plotter wasn't too busy
 """
 
+class IllegalArguments(ValueError):
+    """
+    Just using this to make it clear what the issue is! 
+    """
+    pass
+
+class bhist:
+    """
+    It's a 1D or 2D histogram! 
+    """
+    def __init__(self,edges):
+        """
+        Arg 'edges' should be a tuple of length 1 or 2. Length 1 for 1D hist, and length 2 for 2D hist
+        """
+
+        if not (isinstance(edges, list) or isinstance(edges, tuple) or isinstance(edges, np.ndarray)):
+            raise TypeError("Arg 'edges' must be {}, got {}".format(list, type(edges)))
+
+        if not(len(edges)==1 or len(edges)==2):
+            raise ValueError("Arg 'edges' should be of length 1 or 2, depending on imensionality of desired bhist")
+        for entry in edges:
+            if not (isinstance(entry, list) or isinstance(entry, tuple) or isinstance(entry, np.npdarray)):
+                raise TypeError("Each entry in 'edges' should be list-like, found {}".format(type(entry)))
+            if len(entry)<2:
+                raise ValueError("Entries in 'edges' must be at least length 2, got {}".format(len(entry)))
+        
+        self._edges = np.sort(edges) # each will now be increasing
+        self._fill = np.zeros(np.shape(edges))
+
+        # build the function needed to register additions to the hisograms 
+        if len(edges)==1:
+            def register( amount, where ):
+                index = self._get_loc( where, self._edges )
+                if index is not None:
+                    self._fill[index] += amount 
+            self.register = register
+        
+        else: # length 2
+            def register( amount, xloc, yloc ):
+                xbin = self._get_loc( xloc, self._edges[0] )
+                ybin = self._get_loc( yloc, self._edges[1] )
+                if (xbin is not None) and (ybin is not None):
+                    self._fill[xbin][ybin]+=amount
+            self.register = register
+
+    def _get_loc(value, edges):
+        if value<edges[0] or value>edges[-1]:
+            return
+        else:
+            scan = 0
+            while not (value>=edges[scan] and value<=edges[scan+1]): # remember, the edges are sorted - so this should happen
+                scan += 1
+                if scan==len(edges)-1:
+                    raise Exception("Something bad happened with logic")
+            return(scan)
+            self._fill[scan]+=value 
+
+    @property
+    def centers(self):
+        complete = [ [0.5*(subedge[i+1]+subedge[i]) for i in range(len(subedge)-1)] for subedge in self._edges]
+        return(complete[0] if self._edges==1 else complete)
+    @property
+    def edges(self):
+        complete = [[value for value in subedge] for subedge in self._edges]
+        return(complete[0] if self._edges==1 else complete)
+    @property
+    def widths(self):
+        complete = [[abs(subedges[i+1]-subedges[i]) for i in range(len(subedges)-1)] for subedges in self._edges]
+        return(complete[0] if self._edges==0 else complete)
+    @property
+    def fill(self):
+        complete = [[value for value in subfill] for subfill in self._fill]
+        return(complete if self._fill!=1 else complete[0])
+
 def get_nearest_entry_to( item, array_like):
     """
     This function takes a quantity "item" and a list/array-like item "array_like"
