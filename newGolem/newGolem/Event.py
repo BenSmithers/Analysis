4db8182d6296e4bd7127f2d795a82cbdfaf27eb3
Boggle-Head <b.smithers.actual@gmail.com>
2020-09-29 16:37:51 -0500

    Added event object

---
+++ newGolem/newGolem/Event.py
@@ -0,0 +1,24 @@
+class Event:
+
+    def __init__(self):
+        self.primaryEnergy = 0.0
+        self.primaryAzimuth = 0.0
+        self.primaryZenith = 0.0
+        self.totalColumnDepth = 0.0
+        self.intX = 0
+        self.intY = 0
+
+        self.oneWeight = 0.0
+        self.number_of_generated_mc_events = 0.0
+
+        self.num_events = 0
+
+        self.energy = 0.0
+        self.zenith = 0.0
+        self.azimuth = 0.0
+
+        self.sample = None
+
+        self.topology = 0
+
+        self.year = 0
